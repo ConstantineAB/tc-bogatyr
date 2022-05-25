3fd2a170ab41838bed331f06891ab62988bc20cc
Your Name <you@example.com>
2022-05-25 21:16:51 +0300

    build update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,12 @@ import React, { FC } from 'react';
 import { AppProps } from 'next/app';
 import { wrapper } from '../store';
 import '../styles/globals.css';
-
+import '../styles/Header/header.css';
+import '../styles/Home/home.css';
+import '../styles/Products/products.css';
+import '../styles/Cart/cart.css';
+import '../styles/Footer/footer.css';
+import '../styles/Media/media.css'
 
 const WrappedApp: FC<AppProps> = ({Component, pageProps}) => <Component {...pageProps} />;
 
