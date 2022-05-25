bd41c5f1deae87cbf1bde1c11ea35cb827e40909
Your Name <you@example.com>
2022-05-25 22:49:38 +0300

    make it better

--- store/actions/products.js
+++ store/actions/products.js
@@ -9,7 +9,7 @@ export const fetchProducts = (sortBy, category) => (dispatch) => {
   dispatch(setLoaded(false))
   axios
     .get(
-      `http://localhost:3001/products?${
+      `https://bogatyr-json-server.herokuapp.com:3001/products?${
         category !== null ? `category=${category}` : ''
       }&_sort=${sortBy.type}&_order=${sortBy.order}`,
     )
